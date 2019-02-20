0c148fae67435b008d6ff7665b7da5225fffb197
Mitchel Parish <mitchelparish@gmail.com>
2019-02-20 17:20:26 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -21,7 +21,7 @@ class Application
         @@cart.each do |item|
           resp.write "#{item}/n"
         end
-      end  
+      end
     elsif req.path.match(/add/)
         search_item = req.params["item"]
         if @@items.include?(search_item)
